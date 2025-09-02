542fe34d28c45984b3f58f4b16cf1c39355b15ce
Anshu Pathak <anshukumarpathak1999@gmail.com>
2025-09-03 00:43:34 +0530

    Fixed a bug in flight update and created promise for createBooking

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
-const { InfoController } = require('../../controllers');
+// const { InfoController } = require('../../controllers');
 
 const router = express.Router();
 
-router.get('/info', InfoController.info);
+// router.get('/info', InfoController.info);
+const bookingRoutes = require("./booking-routes");
+router.use("/bookings", bookingRoutes);
 
 module.exports = router;
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,26 +1,26 @@
 const { StatusCodes } = require("http-status-codes");
 const { BookingRepository } = require("../repositories");
 const AppError = require("../utils/errors/app-error");
+const { ServerConfig } = require('../config');
+const axios = require('axios');
+const db = require('../models')
 
 const bookingRepository = new BookingRepository();
 
 async function createBooking(data) {
-  try {
-    const result = await bookingRepository.create(data);
-    return result;
-  } catch (error) {
-    if (error.name === "SequelizeValidationError") {
-      let explanation = [];
-      error.errors.forEach((err) => {
-        explanation.push(err.message);
-      });
-      throw new AppError(explanation, StatusCodes.BAD_REQUEST);
-    }
-    throw new AppError(
-      "Cannot create a new Booking",
-      StatusCodes.INTERNAL_SERVER_ERROR,
-    );
-  }
+    return new Promise((resolve, reject) => {
+      const result = db.sequelize.transaction(async function bookingImplementation(t){
+        const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+        const flightData = flight.data.data.totalSeats;
+        if(data.noOfSeats > flightData){
+          reject(new AppError(
+            "Required number of seats not available",
+            StatusCodes.BAD_REQUEST,
+          ));
+        }
+        resolve(true);
+      })
+    })
 }
 
 async function getBookings() {
